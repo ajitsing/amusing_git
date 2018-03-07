2353e6e3bc5517b2919d93de8b339d26a05d5e86
AjitSingh <jeetsingh.ajit@gmail.com>
2018-03-07 17:58:43 +0530

    Fixed typo in hook name

--- lib/amusing_git/git/git_repository.rb
+++ lib/amusing_git/git/git_repository.rb
@@ -15,7 +15,7 @@ module AmusingGit
     end
 
     def configure_amusing_git!
-      hooks = ["#{@dir}/.git/hooks/pre-push", "#{@dir}/.git/hooks/pre-reabse"].map { |h| AmusingGit::GitHook.new h}
+      hooks = ["#{@dir}/.git/hooks/pre-push", "#{@dir}/.git/hooks/pre-rebase"].map { |h| AmusingGit::GitHook.new h}
       hooks.each do |hook|
         hook.create! unless hook.exists?
         hook.configure_amusing_git! unless hook.amusing?
